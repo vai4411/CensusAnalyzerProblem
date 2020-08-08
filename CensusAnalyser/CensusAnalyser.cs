177334ceed0e0b6d5b574df074fae78e6aee4990
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-08-08 18:20:44 +0530

    ADDED: TC-1.1 check number of records matches

--- CensusAnalyser/CensusAnalyser.cs
+++ CensusAnalyser/CensusAnalyser.cs
@@ -7,13 +7,19 @@ namespace CensusAnalyserProblem
     {
         public static int getCount(string path)
         {
-            int count = 0;
-            string[] n = File.ReadAllLines(path);
-            for(int i = 0; i < n.Length; i++)
+            try
             {
-                count++;
+                int count = 0;
+                string[] n = File.ReadAllLines(path);
+                for (int i = 0; i < n.Length; i++)
+                {
+                    count++;
+                }
+                return count - 1;
+            }catch(Exception e)
+            {
+                throw new CensusAnalyserException("Incorrect File");
             }
-            return count - 1;
         }
     }
 }
